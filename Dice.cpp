91a5529dbe15b067aff437ed0c0f3d6b47c9dc60
Mara Kim <hacker.root@gmail.com>
2013-07-08 02:35:31 -0500

    switch to unsigned ints

--- Dice.cpp
+++ Dice.cpp
@@ -5,7 +5,7 @@
 
 #include "Dice.h"
 
-int Dice::roll(const int& die, const int& times) {
+int Dice::roll(const unsigned int& die, const unsigned int& times) {
     int result = 0;
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
@@ -19,7 +19,7 @@ int Dice::roll(const Dice::roll_type& roll) {
     return Dice::roll(roll.die,roll.times);
 }
 
-Dice::result_type Dice::roll_str(const int& die, const int& times) {
+Dice::result_type Dice::roll_str(const unsigned int& die, const unsigned int& times) {
     int result = 0;
     std::stringstream report;
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
--- Dice.h
+++ Dice.h
@@ -17,14 +17,16 @@ class Dice {
 public:
     typedef std::pair<int,std::string> result_type;
     struct roll_type {
-        int die;
-        int times;
+        unsigned int die;
+        unsigned int times;
     };
 
-    static int roll(const int& die, const int& times);
+    static int roll(const unsigned int& die, const unsigned int& times);
     static int roll(const roll_type& roll);
-    static result_type roll_str(const int& die, const int& times);
+    static result_type roll_str(const unsigned int& die, const unsigned int& times);
     static result_type roll_str(const roll_type& roll);
 };
 
+class Roll {
+};
 #endif // DICE_H
