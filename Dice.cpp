fedb2f0e7ef72f18bfab98394281d814d64c4a82
Mara Kim <hacker.root@gmail.com>
2013-07-09 00:28:39 -0500

    basic roll dropping

--- Dice.cpp
+++ Dice.cpp
@@ -5,42 +5,69 @@
 
 #include "Dice.h"
 
-int Dice::roll(const unsigned int& die, const unsigned int& times) {
-    int result = 0;
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine generator(seed);
-    std::uniform_int_distribution<int> distribution(1,die);
-    for(int i = 0; i < times; i++)
-        result += distribution(generator);
-    return result;
+int Dice::roll(const unsigned int die, const unsigned int times) {
+    Dice::roll_type roll;
+    roll.die = die;
+    roll.times = times;
+    return Dice::roll(roll);
 }
 
 int Dice::roll(const Dice::roll_type& roll) {
-    return Dice::roll(roll.die,roll.times);
-}
-
-Dice::result_type Dice::roll_str(const unsigned int& die, const unsigned int& times) {
-    int result = 0;
-    std::stringstream report;
+    // prepare random generator
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
-    std::uniform_int_distribution<int> distribution(1,die);
+    std::uniform_int_distribution<unsigned int> distribution(1,roll.die);
+    // roll dice
+    std::multiset<unsigned int> set;
+    for(int i = 0; i < roll.times; i++)
+        set.insert(distribution(generator));
+    // calculate result
+    int result = 0,
+        pos = 0,
+        top = set.size()-roll.high;
+    for(auto it = set.begin(); it != set.end() && pos < top; it++) {
+        if(pos >= (roll.low))
+            result += *it;
+        pos++;
+    }
+    return result;
+}
+
+Dice::result_type Dice::roll_str(const unsigned int die, const unsigned int times) {
+    Dice::roll_type roll;
+    roll.die = die;
+    roll.times = times;
+    return Dice::roll_str(roll);
+}
+
+Dice::result_type Dice::roll_str(const Dice::roll_type& roll) {
+    // prepare random generator
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+    std::uniform_int_distribution<unsigned int> distribution(1,roll.die);
+    // roll dice
+    std::multiset<unsigned int> set;
+    for(int i = 0; i < roll.times; i++)
+        set.insert(distribution(generator));
+    // calculate result
+    int result = 0,
+        pos = 0,
+        top = set.size()-roll.high;
     bool first = true;
-    for(int i = 0; i < times; i++) {
-        if(first)
-            first = false;
-        else
-            report << " + ";
-        int roll = distribution(generator);
-        result += roll;
-        report << roll;
+    std::stringstream report;
+    for(auto it = set.begin(); it != set.end() && pos < top; it++) {
+        if(pos >= (roll.low)) {
+            if(first)
+                first = false;
+            else
+                report << " + ";
+            result += *it;
+            report << *it;
+        }
+        pos++;
     }
     Dice::result_type r;
     r.result = result;
     r.report = report.str();
     return r;
-}
-
-Dice::result_type Dice::roll_str(const Dice::roll_type& roll) {
-    return Dice::roll_str(roll.die,roll.times);
 }
--- Dice.h
+++ Dice.h
@@ -11,6 +11,7 @@
 #include <utility>
 #include <string>
 #include <sstream>
+#include <set>
 #include "Options.h"
 
 class Dice {
@@ -22,11 +23,13 @@ public:
     struct roll_type {
         unsigned int die;
         unsigned int times;
+        unsigned int high;
+        unsigned int low;
     };
 
-    static int roll(const unsigned int& die, const unsigned int& times);
+    static int roll(const unsigned int die, const unsigned int times);
     static int roll(const roll_type& roll);
-    static result_type roll_str(const unsigned int& die, const unsigned int& times);
+    static result_type roll_str(const unsigned int die, const unsigned int times);
     static result_type roll_str(const roll_type& roll);
 };
 
