f43740e3960c514aafd6d62a164e181379ab52c2
Mara Kim <hacker.root@gmail.com>
2013-07-13 10:08:40 -0500

    developed responses

--- Personality.cpp
+++ Personality.cpp
@@ -5,7 +5,14 @@
 
 #include "Personality.h"
 
-Personality::response_pool Personality::_name_response {"That's me!","<3"};
+Personality::response_pool Personality::_name_response
+   {"<3","<3","<3","<3",
+    "<3","<3","<3","<3",
+    ":D",";D",";)",":O",
+    "That's me!",
+    "Sup.",
+    "xoxo",
+    "Awww, youu!"};
 
 void Personality::respond_name() {
     std::cout << _name_response[Dice::roll(_name_response.size(),1)-1] << '\n';
