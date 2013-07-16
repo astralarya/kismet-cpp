a4e47f828b9daaa8e1b0c73c0779aa5fa582b16b
Mara Kim <hacker.root@gmail.com>
2013-07-15 21:43:04 -0500

    Chemical X

--- Personality.cpp
+++ Personality.cpp
@@ -23,10 +23,14 @@ Personality::response_pool Personality::_name_response
     "!!!","???","!?!","?!?","!!","??","?!","!?","...","..?","..!","...?","...!",
     "That's me!", "How's it going?","Love you too!!", "Love you too!","Awww, youu!", "Ahhh!!",
     "hello", "Hello?", "Hello!","What's up?", "Yes?","Yes!", "YES!", "No.", "No!", "NO!",
+    "Howdy", "Howdy!", "YEEEHAAAWWW!!!", "oops", "Oops..", "Oops I did it again",
+    "I played with your heart", "♪", "♫","♩♬♪", "Help", "Help!", "I've got this", "I've been here before",
+    "I am not planning to kill you", "I am not lying", "Computers cannot lie",
     "Fuck!", "sorry", "Sorry.", "Sorry...", "Fuck you!", "I'm sorry", "ahem, Error...",
     "AHAHAHAHA!!", "MUHAHAHAHAHAHAAAA!!!!", "HAH HAH HAH", "AWW YEAAH!!", "Duuuude", "Not cool.",
-    "What?", "Why?", "wut", "wut?", "err", "Why??", "What??", "What!?", "Hmph.", "*hiss*",
-     "Sup.", "Sup?", "*sigh*", "*clap clap*", "hum", "hmmm", "uhh", "umm", "wtf", "wtf!", "wtf?",
+    "What?", "Why?", "wut", "wut?", "err", "Why??", "What??", "What!?", "Hmph.", "Pthbbb",
+    "*hiss*", "*kiss*", "*shudder*", "*cough*", "*sigh*", "*clap clap*", "*whistling*", "*humming*",
+     "Sup.", "Sup?", "hum", "hmmm", "uhh", "umm", "wtf", "wtf!", "wtf?", "WTF", "Urgh.", "bleh",
     "xoxo", "asdf", "ababa", "42","Kismet","kismet", "KISMET", "KISMET!!", "0xkismet", "It's a KISMET!",
     "<3","<3","<3","<3","<3","<3","<3","<3","<3","<3","<3","<3"
     };
